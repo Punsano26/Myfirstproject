cfbd46dc34bf7da0ac8e7445c920f8d609f2378e
Worachet Uttha <wuttha@webmail.npru.ac.th>
2024-03-07 16:55:37 +0700

    first commit

---
+++ calculator.test.js
@@ -0,0 +1,23 @@
+const mathOperation = require("./calculator");
+
+describe("Calculator test", () = > {
+    test("adding 1 + 2 should return 3", () => {
+        expect(mathOperation.sum(1, 2)).toBe(3);
+    });
+});
+
+const mathOperation = require("./calculator");
+
+describe("Calculator test", () = > {
+    test("subtracting 1 + 2 should return 3", () => {
+        expect(mathOperation.sum(1, 2)).toBe(3);
+    });
+});
+
+const mathOperation = require("./calculator");
+
+describe("Calculator test", () = > {
+    test("adding 1 + 2 should return 3", () => {
+        expect(mathOperation.sum(1, 2)).toBe(3);
+    });
+});
